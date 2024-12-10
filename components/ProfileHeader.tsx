b7c606d7d38affe25b732cb45fcfe5e0a699bc8c
h4ydn-n9uy3n <h4ydn.n9uy3n@gmail.com>
2024-12-09 21:34:38 -0600

    Make the play button visible again in ProfileHeader

--- components/ProfileHeader.tsx
+++ components/ProfileHeader.tsx
@@ -332,7 +332,7 @@ const ProfileHeader = ({
       </div>
 
       <div style={{ maxWidth: '300px', margin: '0 auto' }}>
-        <video ref={videoRef} controls style={{ width: '100%', display: 'none' }}>
+        <video ref={videoRef} controls style={{ width: '100%' }}>
           <source src="/videos/setnhac.mp4" type="video/mp4" />
           Your browser does not support the video tag.
         </video>
